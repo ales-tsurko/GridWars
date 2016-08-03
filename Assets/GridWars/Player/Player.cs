e2ae6efebf58db9bf714ac1b5a209fabec5511da
Rich Collins <richcollins@gmail.com>
2016-08-02 18:16:54 -0700

    Player

---
+++ Assets/GridWars/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	PowerBar powerBar;
+	Color color;
+	int playerNumber;
+
+	// Use this for initialization
+	void Start () {
+		powerBar = PowerBar.New();
+		powerBar.SetColor(color);
+		powerBar.SetPosition(playerNumber == 1 ? PowerBarPlacement.Left : PowerBarPlacement.Right);
+		powerBar.minPower = 0f;
+		powerBar.maxPower = 600f;
+		powerBar.power = 0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		powerBar.power += 1;
+	}
+}
---
+++ Assets/UI/Scripts/GameUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameUI : MonoBehaviour {
+
+	private static GameUI _instance;
+	public static GameUI instance {
+		get {
+			if (_instance == null) {
+				_instance = FindObjectOfType<GameUI> ();
+				if (_instance == null) {
+					GameObject ui = (GameObject)Instantiate (Resources.Load<GameObject> ("GameUI"));
+					ui.name = "GameUI";
+					_instance = ui.GetComponent<GameUI> ();
+				}
+			}
+			return _instance;
+		}
+	}
+
+	public RectTransform topPanel;
+	public static RectTransform GetUIParent () {
+		return instance.topPanel;
+	}
+
+}
