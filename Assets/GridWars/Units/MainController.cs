ceeb35da6113f7bee53f9e97c41683b65c8ca851
Steve Dekorte <steve@dekorte.com>
2016-08-04 16:36:59 -0700

    prefabs

---
+++ Assets/GridWars/Units/MainController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Tower tower = Instantiate(Resources.Load("Tower")) as Tower; 
+		tower.setX (0.0f);
+		tower.setZ (0.0f);
+		tower.setY (4.0f);
+	}
+}
---
+++ Assets/GridWars/Units/Tower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tower : GameUnit {
+	void Start () {
+
+
+	}
+
+
+
+	public override void FixedUpdate () {
+
+
+	}
+
+}
