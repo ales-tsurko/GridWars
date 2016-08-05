033b9929dcd35314fac7cf7bc1496b8c64a62265
Michael Uzdavines <mike@uwattorneys.com>
2016-08-04 23:08:55 -0400

    first draft of tire tracks

---
+++ Assets/TireTracks/Scripts/TireTrackGenerator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TireTrackGenerator : MonoBehaviour {
+	int lastMark;
+	Transform _t;
+	void Start (){
+		_t = transform;
+	}
+	void Update(){
+		lastMark = TireTracks.instance.AddSkidMark (_t.position, new Vector3 (0, 1, 0), 1, lastMark);
+	}
+}
