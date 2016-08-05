f9524c7e9cfaa8bc8dbbe2eb7a38aa8f67820338
Steve Dekorte <steve@dekorte.com>
2016-08-04 18:05:30 -0700

    init towers working

--- Assets/GridWars/GameManager.cs
+++ Assets/GridWars/GameManager.cs
@@ -5,9 +5,9 @@ public class GameManager : MonoBehaviour {
 	List<Player> players;
 
 	public GameObject prefabTower;
+	public GameObject prefabChopper;
 	public GameObject prefabTank;
 	public GameObject prefabLightTank;
-	public GameObject prefabChopper;
 	public GameObject prefabJeep;
 
 	// Use this for initialization
@@ -23,18 +23,36 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void SetupTowers() {
-
 		//GameObject towerGameObject = Instantiate(Resources.Load("Tower")) as GameObject;
-		GameObject towerGameObject = Instantiate(prefabTower); 
 
-		Tower tower = towerGameObject.GetComponent<Tower> ();
-
+		var prefabs = new List <GameObject> { prefabChopper, prefabTank, prefabLightTank, prefabJeep };
 
 
+		for (int playerNum = 0; playerNum < 2; playerNum ++) {
+			float z = 35*(playerNum == 0 ? -1 : 1);
 
-		tower.setX (10.0f);
-		tower.setZ (10.0f);
-		tower.setY (14.0f);
+			int maxTowers = 4;
+			for (int towerNum = 0; towerNum < maxTowers; towerNum ++) {
+				GameObject towerGameObject = Instantiate(prefabTower); 
+				Tower tower = towerGameObject.GetComponent<Tower> ();
+
+				if (playerNum == 0) {
+					tower.prefabUnit = prefabs [towerNum];
+				} else {
+					tower.prefabUnit = prefabs [maxTowers - 1 - towerNum];
+				}
+
+				float x = 50*((((float)towerNum) / (float)maxTowers) - 0.5f);
+				tower.setX (x);
+				tower.setY (0.0f);
+				tower.setZ (z);
+				tower.setRotY (180*playerNum);
+				tower.playerNumber = playerNum + 1;
+				tower.constructUnit ();
+			}
+		}
+
+
 		//print ("created tower " + tower);
 	}
 	
--- Assets/GridWars/Units/Tower.cs
+++ Assets/GridWars/Units/Tower.cs
@@ -3,11 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Tower : GameUnit {
+	public GameObject prefabUnit;
+	public int playerNumber;
+
 	void Start () {
 
 
 	}
 
+	public void constructUnit () {
+		GameObject unitGameObject = Instantiate(prefabUnit); 
+		GameUnit unit = unitGameObject.GetComponent<GameUnit> ();
+		unit.setX(transform.position.x);
+		unit.setZ(transform.position.z);
+		string tagString = "Player" + playerNumber;
+		unit.tag = tagString;
+		unit.setRotY (rotY());
+
+	}
 
 
 	public override void FixedUpdate () {
