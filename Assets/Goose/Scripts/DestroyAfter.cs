df3a8124133003421015e71106384a06317b9fbe
Michael Uzdavines <mike@uwattorneys.com>
2016-08-10 09:45:51 -0700

    Destroy objects after x Seconds

---
+++ Assets/Goose/Scripts/DestroyAfter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfter : MonoBehaviour {
+
+	public float destroyTime = 10f;
+	void Start () {
+		Destroy (gameObject, destroyTime);
+	}
+}
