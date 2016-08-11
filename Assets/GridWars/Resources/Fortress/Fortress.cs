d6cba830ab1a2311bfbcc4b4c5079f6a50c14e1a
Steve Dekorte <steve@dekorte.com>
2016-08-10 17:10:23 -0700

    work on game end, new assets

--- Assets/GridWars/Resources/Fortress/Fortress.cs
+++ Assets/GridWars/Resources/Fortress/Fortress.cs
@@ -96,4 +96,18 @@ public class Fortress : MonoBehaviour {
 			gameUnit.GetComponent<Engineer> ().SwitchState (Engineer.State.Init);
 		}
 	}
+
+	public virtual bool IsDead() {
+		return false;
+		/*
+		foreach (Tower tower in towers) {
+			if (!tower.gameObect.IsDestroyed()) {
+				return false;
+			}
+		}
+
+
+		return true;
+		*/
+	}
 }
