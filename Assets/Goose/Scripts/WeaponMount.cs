03392064f5e3805da20664d324f46224bb7a436c
Michael Uzdavines <mike@uwattorneys.com>
2016-08-15 20:36:39 -0400

    Weapon Mount Example

---
+++ Assets/Goose/Scripts/WeaponMount.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponMount : MonoBehaviour {
+	public GameObject weapon;
+
+	void Start () {
+		GameObject _weapon = (GameObject)Instantiate (weapon);
+		_weapon.name = weapon.name;
+		_weapon.transform.parent = transform;
+		_weapon.transform.localPosition = Vector3.zero;
+		_weapon.transform.localRotation = Quaternion.Euler (Vector3.zero);
+		_weapon.transform.parent = transform.parent;
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
