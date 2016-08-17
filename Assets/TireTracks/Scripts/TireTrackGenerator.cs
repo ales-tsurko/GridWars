54a9287bb005d008b1ac72792b14f2f1a875d785
Michael Uzdavines <mike@uwattorneys.com>
2016-08-17 13:59:51 -0400

    Tire Track consistency fix

--- Assets/TireTracks/Scripts/TireTrackGenerator.cs
+++ Assets/TireTracks/Scripts/TireTrackGenerator.cs
@@ -13,12 +13,24 @@ public class TireTrackGenerator : MonoBehaviour {
 		_t = transform;
 	}
 	void Update(){
-		lastMark = track.AddSkidMark (_t.position, new Vector3 (0, 1, 0), 1, lastMark);
+		lastMark = track.AddSkidMark (new Vector3 (_t.position.x, 0.02f, _t.position.z), new Vector3 (0, 1, 0), 1, lastMark);
 	}
 
 	void OnDestroy(){
 		if (track != null) {
 			track.FadeOutAndDie ();
 		}
+	}
+}
+
+public static class TireTrackGeneratorExtension {
+	public static Vector3 SurfacePosition (this Transform t, Terrain ter = null){
+		if (ter == null) {
+			ter = GameObject.FindObjectOfType<Terrain> ();
+		}
+		if (ter == null) {
+			return t.position;
+		}
+		return new Vector3 (t.position.x, ter.SampleHeight (t.position), t.position.z);
 	}
 }
