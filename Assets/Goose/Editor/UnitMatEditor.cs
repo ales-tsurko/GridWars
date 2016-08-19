6550d05e543b863cb9f4c013284b669ef0e911ac
Michael Uzdavines <mike@uwattorneys.com>
2016-08-19 09:28:39 -0400

    Editor Window to apply Material to all children of a GO

---
+++ Assets/Goose/Editor/UnitMatEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+public class UnitMatEditor : EditorWindow {
+
+	[MenuItem ("Units/Material Applicator")]
+	static void Init () {
+		// Get existing open window or if none, make a new one:
+		UnitMatEditor window = (UnitMatEditor)EditorWindow.GetWindow<UnitMatEditor> (typeof(SceneView));
+		window.Show();
+	}
+
+	Material mat;
+	GameObject go;
+
+	void OnGUI () {
+		GUILayout.Label ("Apply Material to all children", EditorStyles.boldLabel);
+		mat = (Material)EditorGUILayout.ObjectField(mat, typeof(Material));
+		go = (GameObject)EditorGUILayout.ObjectField (go, typeof(GameObject));
+		if (GUILayout.Button ("Apply")) {
+			Apply ();
+		}
+	}
+
+	void Apply () {
+		Renderer[] rends = go.GetComponentsInChildren<Renderer> ();
+		foreach (Renderer rend in rends) {
+			if (rend.GetComponent<ParticleSystem> ()) {
+				continue;
+			}
+			rend.material = mat;
+		}
+		this.Close ();
+	}
+
+
+}
