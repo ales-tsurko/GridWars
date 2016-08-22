bd0119863278ca8033339e981250f7fc639a2da0
Steve Dekorte <steve@dekorte.com>
2016-08-22 14:29:07 -0700

    App, TimerCenter and Timer classes added

---
+++ Assets/GridWars/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace AssemblyCSharp {
+	public class Timer {
+		public TimerCenter timerCenter;
+		public object target;
+		public string method;
+		public float fireTime; 
+
+		public Timer() {
+		}
+
+		public Timer SetTimeout(float secs) {
+			fireTime = Time.time + secs;
+			return this;
+		}
+
+		public Timer SetTartget(object aTarget) {
+			target = aTarget;
+			return this;
+		}
+
+		public Timer SetMethod(string aMethod) {
+			method = aMethod;
+			return this;
+		}
+
+		public bool IsReady() {
+			return (Time.time >= fireTime);
+		}
+
+		public void Send() {
+			Type targetType = target.GetType();
+			MethodInfo methodInfo = targetType.GetMethod(method);
+			methodInfo.Invoke(target, null);
+
+			/*
+			MethodInfo methodInfo = target.GetType().GetMethod(method);
+			MethodInfo generic = methodInfo.MakeGenericMethod(target.GetType());
+			generic.Invoke(this, null);
+			*/
+		}
+
+		public void Start() {
+			timerCenter.AddTimer(this);
+		}
+				
+	}
+}
+
---
+++ Assets/GridWars/TimerCenter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/*
+ * example use:
+ *   App.shared.timerCenter.NewTimer().SetTimeout(2.0f).SetTarget(this).SetMethod("timeout").Start();
+ * 
+ * 
+ */ 
+
+namespace AssemblyCSharp {
+	public class TimerCenter {
+		List <Timer> timers;
+
+		public TimerCenter() {
+			timers = new List<Timer>();
+		}
+
+		public Timer NewTimer() {
+			Timer timer = new Timer();
+			timer.timerCenter = this;
+
+			return timer;
+		}
+
+		public void AddTimer(Timer newTimer) {
+			int index = 0;
+			foreach (Timer timer in timers) {
+				if (newTimer.fireTime < timer.fireTime) {
+					timers.Insert(index, newTimer);
+					index++;
+					return;
+				}
+			}
+
+			timers.Add(newTimer);
+		}
+
+		public void Step() {
+			while (timers.Count > 0) {
+				Timer timer = timers[0];
+				if (timer.IsReady()) {
+					timer.Send();
+					timers.RemoveAt(0);
+				} else {
+					return;
+				}
+			}
+		}
+	}
+}
+
