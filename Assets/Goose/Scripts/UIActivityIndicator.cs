a8eef018725d441022a58d245d55c78baad9b5f2
Michael Uzdavines <mike@uwattorneys.com>
2016-08-30 14:57:32 -0400

    UI Updates

---
+++ Assets/Goose/Scripts/UIActivityIndicator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class UIActivityIndicator : UIElement {
+	public Image image;
+	public float rotateSpeed;
+	Text text;
+
+	void Update () {
+		transform.GetComponentInChildren<Text> ().rectTransform.rotation = Quaternion.Euler (Vector3.zero);
+		transform.RotateAround (transform.forward, Time.deltaTime * rotateSpeed);
+	}
+}
--- Assets/Goose/Scripts/UIElement.cs
+++ Assets/Goose/Scripts/UIElement.cs
@@ -1,21 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
 public class UIElement : MonoBehaviour {
 
 	/// <summary>
 	/// Sets the text of the GameButton
 	/// </summary>
 	/// <param name="s">S.</param>
-	public void SetText (string s){
-		Text textObj = GetComponentInChildren<Text> ();
+	public virtual void SetText (string s, float offset = 0){
+		Text textObj = null;
+		RectTransform _t = GetComponent<RectTransform> ();
+		textObj = GetComponentInChildren<Text> ();	
 		if (textObj == null) {
-			Debug.LogError (transform.name + " has no text element");
-			return;
+			textObj = UI.CreateTextObj (_t).GetComponent<Text>();
 		}
 		textObj.text = s;
 	}
 
+	public System.Object data;
+	public System.Action<UIMenuItem> action;
 	/// <summary>
 	/// Sets the position of the Button
 	/// </summary>
@@ -27,7 +32,37 @@ public class UIElement : MonoBehaviour {
 		transform.localPosition = new Vector3 (x * w / 2f, h * y / 2f, 0);
 	}
 
-	public void SetSize (float x, float y){
+	public void SetSize (float x, float y, bool preserveAspect = true){
 		GetComponent<RectTransform> ().sizeDelta = new Vector2 (x, y);
+		SetImageAspect (preserveAspect);
+	}
+
+	public void Show () {
+		gameObject.SetActive (true);
+	}
+
+	public void Hide () {
+		gameObject.SetActive (false);
+	}
+
+	public void SetImageAspect (bool b){ 
+		if (GetComponent<Image> () == null) {
+			return;
+		}
+		GetComponent<Image> ().preserveAspect = b;
+	}
+	public void SetImageType (Image.Type type) {
+		if (GetComponent<Image> () == null) {
+			return;
+		}
+		GetComponent<Image> ().type = type;
+	}
+
+	/// <summary>
+	/// Sets the method to call OnClick
+	/// </summary>
+	/// <param name="action">Method without parens</param>
+	public void SetAction (System.Action<UIMenuItem> _action){
+		action = _action;
 	}
 }
