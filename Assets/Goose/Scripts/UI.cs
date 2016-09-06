a9b71af3d0dcb33c3cc11fbeae08df85f7fbfc90
Michael Uzdavines <michaeluzdavines@Michaels-MBP.attlocal.net>
2016-09-06 16:54:45 -0400

    UI edits, plain text

--- Assets/Goose/Scripts/UI.cs
+++ Assets/Goose/Scripts/UI.cs
@@ -10,24 +10,41 @@ public static class UI {
 	const string BUTTON = DIR + "Button";
 	const string CANVAS = "Canvas";
 	const string SKINDIR = DIR + "Skins/";
+	const string FONTDIR = DIR + "Fonts/";
+	const UIFont DEFAULTFONT = UIFont.Army;
 
-	static UIButton Button (string title, System.Action<UIMenuItem> action, MenuItemType type, string skin){
+	static UIButton Button (string title, System.Action<UIMenuItem> action, MenuItemType type, string skin, bool animated){
 		skin += "/";
-		GameObject go = new GameObject ();
+		GameObject go;
+		if (animated) {
+			go = MonoBehaviour.Instantiate (Resources.Load<GameObject> ("UI/AnimatedButton"));
+		} else {
+			go = new GameObject ();
+		}
 		UIButton button = go.AddComponent<UIButton> ();
 		AssignToCanvas (go);
-		Sprite sprite = Resources.Load<Sprite> (SKINDIR + skin + type.ToString());
-		button.GetComponent<Image> ().overrideSprite = sprite;
+		Image image = button.GetComponent<Image> ();
+		if (image == null) {
+			image = button.GetComponent<Image> ();
+		}
+		if (type != MenuItemType.ButtonTextOnly) {
+			Sprite sprite = Resources.Load<Sprite> (SKINDIR + skin + type.ToString ());
+			image.overrideSprite = sprite;
+		} else {
+			image.overrideSprite = null;
+			image.color = new Color (1f, 1f, 1f, 0f);
+		}
 		button.SetAction(action);
 		button.SetText(title);
 		return button;
 	}
 
-	public static UIMenuItem MenuItem (string title = "Button", System.Action<UIMenuItem> action = null, MenuItemType type = MenuItemType.ButtonRound, string skin = "Default"){
+	public static UIMenuItem MenuItem (string title = "Button", System.Action<UIMenuItem> action = null, MenuItemType type = MenuItemType.ButtonTextOnly, string skin = "Default", bool animated = true){
 		switch (type) {
 		case MenuItemType.ButtonRound:
 		case MenuItemType.ButtonSquare:
-			return Button (title, action, type, skin);
+		case MenuItemType.ButtonTextOnly:
+			return Button (title, action, type, skin, animated);
 		}
 		return null;
 	}
@@ -52,28 +69,34 @@ public static class UI {
 		indicator.name = "ActivityIndicator";
 		return indicator;
 	}
-		
+	public static Font UseFont (UIFont _font) {
+		Debug.Log (FONTDIR + _font.ToString ());
+		return Resources.Load<Font> (FONTDIR + _font.ToString ());
+	}
 	/// <summary>
 	/// Returns the Canvas or creates one if null
 	/// </summary>
 	/// <returns>The canvas.</returns>
 	public static Canvas MainCanvas () {
 		GameObject go = GameObject.Find (CANVAS);
-		Canvas canvas;
 		if (go == null) {
-			go = new GameObject ();
-			canvas = go.AddComponent<Canvas> ();
-			canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-			canvas.name = CANVAS;
-			//var scaler = canvas.gameObject.AddComponent<CanvasScaler> ();
-			var raycaster = canvas.gameObject.AddComponent<GraphicRaycaster> ();
-			raycaster.blockingObjects = GraphicRaycaster.BlockingObjects.None;
-			var eventSystem = new GameObject ().AddComponent<EventSystem> ();
-			eventSystem.gameObject.AddComponent<StandaloneInputModule> ();
-			return canvas;
-		} else {
-			return go.GetComponent<Canvas> ();
+			go = MonoBehaviour.Instantiate (Resources.Load<GameObject> ("UI/Canvas"));
+			if (go == null) {
+				Canvas canvas;
+				go = new GameObject ();
+				canvas = go.AddComponent<Canvas> ();
+				canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+				canvas.name = CANVAS;
+				//var scaler = canvas.gameObject.AddComponent<CanvasScaler> ();
+				var raycaster = canvas.gameObject.AddComponent<GraphicRaycaster> ();
+				raycaster.blockingObjects = GraphicRaycaster.BlockingObjects.None;
+				var eventSystem = new GameObject ().AddComponent<EventSystem> ();
+				eventSystem.gameObject.AddComponent<StandaloneInputModule> ();
+				return canvas;
+			}
 		}
+		go.name = CANVAS;
+		return go.GetComponent<Canvas> ();
 	}
 
 	static void AssignToCanvas (GameObject go){
@@ -82,7 +105,7 @@ public static class UI {
 		go.transform.localScale = Vector3.one;
 	}
 
-	public static RectTransform CreateTextObj (this RectTransform _parent){
+	public static RectTransform CreateTextObj (this RectTransform _parent, UIFont _font = DEFAULTFONT){
 		GameObject textObj = new GameObject ();
 		var rect = textObj.AddComponent<RectTransform> ();
 		rect.SetParent (_parent);
@@ -91,11 +114,16 @@ public static class UI {
 		rect.sizeDelta = _parent.GetComponent<RectTransform> ().sizeDelta * .5f;
 		Text text = textObj.AddComponent<Text> ();
 		text.resizeTextForBestFit = true;
-		text.font = Resources.GetBuiltinResource<Font> ("Arial.ttf");
+		if (_font == UIFont.None) {
+			text.font = Resources.GetBuiltinResource<Font> ("Arial.ttf");
+		} else {
+			text.font = UseFont (_font);
+		}
 		text.alignment = TextAnchor.MiddleCenter;
 		return textObj.GetComponent<RectTransform> ();
 	}
 }
 
-public enum MenuItemType {ButtonRound, ButtonSquare, Label, TextField}
+public enum MenuItemType {ButtonRound, ButtonSquare, Label, TextField, ButtonTextOnly}
+public enum UIFont {None, Army}
 public class UIMenuItem : UIElement {}
