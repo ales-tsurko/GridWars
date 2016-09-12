11121874f5fefa8d50f447453220066f7112ba2f
Michael Uzdavines <michaeluzdavines@Michaels-MacBook-Pro.local>
2016-09-12 16:46:22 -0400

    Ground Blast Effect fix - will disable itself if in the air, if near the ground will set itself to y = .05f

---
+++ Assets/Goose/Scripts/GroundEffect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundEffect : MonoBehaviour {
+	public float threshold;
+
+	void Update(){
+		if (transform.parent.position.y > threshold) {
+			gameObject.SetActive (false);
+		} else {
+			transform.localPosition = new Vector3 (0, -transform.parent.position.y + .05f, 0);
+		}
+	}
+}
