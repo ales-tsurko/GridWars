c58310af35d8a1b01f6f6f11373f0eb6c56ff6c2
Rich Collins <richcollins@gmail.com>
2016-09-27 14:22:24 -0700

    better ActivityIndicator dots

--- Assets/Goose/Scripts/UIActivityIndicator.cs
+++ Assets/Goose/Scripts/UIActivityIndicator.cs
@@ -4,33 +4,41 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class UIActivityIndicator : UIMenuItem {
 	string prefix;
-	int updateCount = 0;
 	int maxDots = 10;
+	int dotCount = 0;
+	float showTime = 0f;
 
 	public Text SetText(string text) {
 		prefix = text + "\n";
 		return base.SetText(prefix);
 	}
 
+	public override void Show() {
+		base.Show();
+
+		showTime = Time.time;
+	}
+
 	void Update () {
-		if (Time.frameCount % 60 == 0) {
+		var newDotCount = Mathf.FloorToInt(Time.time - showTime) % (maxDots + 1);
+
+		if (dotCount != newDotCount) {
+			dotCount = newDotCount;
 			UpdateText();
 		}
+
 		//transform.GetComponentInChildren<Text> ().rectTransform.rotation = Quaternion.Euler (Vector3.zero);
 		//transform.Rotate (transform.forward, Time.deltaTime * rotateSpeed);
 	}
 
 	void UpdateText() {
 		var suffix = "";
-		var limit = updateCount % (maxDots + 1);
 
-		for (var i = 0; i < limit; i ++) {
+		for (var i = 0; i < dotCount; i ++) {
 			suffix += ".";
 		}
 
 		transform.GetComponentInChildren<Text>().text = prefix + suffix;
-
-		updateCount ++;
 	}
 
 }
