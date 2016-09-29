1c61b8b8694950af9263553a043ab75233fb66d8
Rich Collins <richcollins@gmail.com>
2016-09-28 22:51:10 -0700

    Flashing Tanker.

--- Assets/GridWars/Resources/GameUnit/Vehicle/GroundVehicle/Tanker/Tanker.cs
+++ Assets/GridWars/Resources/GameUnit/Vehicle/GroundVehicle/Tanker/Tanker.cs
@@ -50,6 +50,32 @@ public class Tanker : GroundVehicle {
 		}
 	}
 
+	public override void ServerAndClientUpdate() {
+		base.ServerAndClientUpdate();
+
+		var period = 1;
+		var min = 0.25f;
+		var max = 0.75f;
+		//var value = Mathf.Sin(2*Mathf.PI*Time.time/period);
+
+		var x = Time.time;
+
+
+		//var y = 1 - 2*Mathf.Abs(Mathf.Round(x/period) - x/period); //triangle sawtooth
+		var y = Mathf.Sin(2*Mathf.PI*x/period); //sin
+
+		var intensity = y*(max - min) + min;
+
+		var color = player.primaryColor*Mathf.LinearToGammaSpace(intensity);
+
+		gameObject.EachMaterial(m => {
+			if (m.name.StartsWith(Player.primaryColorMaterialName)) {
+				//Debug.Log(color);
+				m.SetColor("_EmissionColor", color);
+			}
+		});
+	}
+
 	override public bool HasWeapons() {
 		return true;
 	}
