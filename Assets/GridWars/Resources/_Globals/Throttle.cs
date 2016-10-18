bd85fea41ba57a75c581c63c62e3df820ef4b7ae
Steve Dekorte <steve@dekorte.com>
2016-10-17 22:58:42 -0700

    small Throttle optimization - cache bucket

--- Assets/GridWars/Resources/_Globals/Throttle.cs
+++ Assets/GridWars/Resources/_Globals/Throttle.cs
@@ -3,17 +3,28 @@ using System.Collections;
 
 public class Throttle {
 	public MonoBehaviour behaviour;
-	public int period;
-
+	public int _period;
+	public int _bucket;
+		
 	public Throttle() {
 		app = App.shared; //perf opt
 	}
 
+	public int period {
+		set {
+			_period = value;
+			_bucket = (int)((uint)behaviour.GetHashCode() % (uint)period);
+		}
+		get {
+			return _period;
+		}
+	}
+
 	public bool isOff {
 		get {
-			var bucket = (int)((uint)behaviour.GetHashCode() % (uint)period);
+			//var bucket = (int)((uint)behaviour.GetHashCode() % (uint)period);
 			//Debug.Log(App.shared.timeCounter + " " + period + " " + bucket);
-			return (app.timeCounter % period == bucket);
+			return (app.timeCounter % period == _bucket);
 		}
 	}
 
