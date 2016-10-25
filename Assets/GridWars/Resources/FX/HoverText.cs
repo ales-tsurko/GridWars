9890371c3e87957f01d3aa33125455277b34d172
Steve Dekorte <steve@dekorte.com>
2016-10-25 02:58:24 -0700

    HoverText working

--- Assets/GridWars/Resources/FX/HoverText.cs
+++ Assets/GridWars/Resources/FX/HoverText.cs
@@ -5,8 +5,8 @@ using System.Collections;
 
 public class HoverText : MonoBehaviour {
 
-	public Transform target;		// Object that this label should follow
-	Vector3 offset = Vector3.up * 5f;	// Units in world space to offset; 1 unit above object by default
+	public Transform target;			// Object that this label should follow
+	//Vector3 offset = Vector3.up * 5f;	// Units in world space to offset; 1 unit above object by default
 	public Camera cam = null;
 
 	public void Start () {
@@ -16,12 +16,22 @@ public class HoverText : MonoBehaviour {
 	}
 
 	public void Update () {
-		Transform pt = transform.parent;
+		/*
+		Transform pt = transform;
 		pt.position = cam.WorldToViewportPoint(target.position + offset);
 
 		Vector3 v = cam.transform.position - pt.position;
 		v.x = v.z = 0.0f;
-		pt.LookAt( cam.transform.position - v ); 
+		pt.LookAt(cam.transform.position - v); 
 		pt.Rotate(0, 180, 0);
+		*/
+
+		Vector3 v = cam.transform.position - transform.position; 
+		transform.rotation = Quaternion.LookRotation( v );
+	
+
+
+		//transform.rotation = Quaternion.LookRotation(n) * Quaternion.Euler(0, 90, 0);
+
 	}
 }
