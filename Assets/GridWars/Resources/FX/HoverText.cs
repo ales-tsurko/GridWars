4ba4e87e932bbacbb4336c8f03122c328119d72c
Steve Dekorte <steve@dekorte.com>
2016-10-25 02:43:08 -0700

    HoverText

--- Assets/GridWars/Resources/FX/HoverText.cs
+++ Assets/GridWars/Resources/FX/HoverText.cs
@@ -6,38 +6,22 @@ using System.Collections;
 public class HoverText : MonoBehaviour {
 
 	public Transform target;		// Object that this label should follow
-	Vector3 offset = Vector3.up;	// Units in world space to offset; 1 unit above object by default
-	bool clampToScreen = false;	// If true, label will be visible even if object is off screen
-	float clampBorderSize = .05f;	// How much viewport space to leave at the borders when a label is being clamped
-	bool useMainCamera = true;	// Use the camera tagged MainCamera
-	Camera cameraToUse;	// Only use this if useMainCamera is false
-	private Camera cam;
-	private Transform thisTransform;
-	private Transform camTransform;
+	Vector3 offset = Vector3.up * 5f;	// Units in world space to offset; 1 unit above object by default
+	public Camera cam = null;
 
 	public void Start () {
-		thisTransform = transform;
-
-		if (useMainCamera) {
+		if (cam == null) {
 			cam = Camera.main;
-		} else {
-			cam = cameraToUse;
 		}
-
-		camTransform = cam.transform;
 	}
 
 	public void Update () {
-		if (clampToScreen) {
-			var relativePosition = camTransform.InverseTransformPoint(target.position);
-			relativePosition.z = Mathf.Max(relativePosition.z, 1.0f);
-			thisTransform.position = cam.WorldToViewportPoint(camTransform.TransformPoint(relativePosition + offset));
-			thisTransform.position = new Vector3(Mathf.Clamp(thisTransform.position.x, clampBorderSize, 1.0f - clampBorderSize),
-				Mathf.Clamp(thisTransform.position.y, clampBorderSize, 1.0f - clampBorderSize),
-				thisTransform.position.z);
-		}
-		else {
-			thisTransform.position = cam.WorldToViewportPoint(target.position + offset);
-		}
+		Transform pt = transform.parent;
+		pt.position = cam.WorldToViewportPoint(target.position + offset);
+
+		Vector3 v = cam.transform.position - pt.position;
+		v.x = v.z = 0.0f;
+		pt.LookAt( cam.transform.position - v ); 
+		pt.Rotate(0, 180, 0);
 	}
 }
