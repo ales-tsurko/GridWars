146ad5e17dd2b0f95964ea00163bb9f12660dbbd
Steve Dekorte <steve@dekorte.com>
2016-10-25 02:21:09 -0700

    starting work on hover text

---
+++ Assets/GridWars/Resources/FX/HoverText.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// attach this to a GUIText
+
+public class HoverText : MonoBehaviour {
+
+	public Transform target;		// Object that this label should follow
+	Vector3 offset = Vector3.up;	// Units in world space to offset; 1 unit above object by default
+	bool clampToScreen = false;	// If true, label will be visible even if object is off screen
+	float clampBorderSize = .05f;	// How much viewport space to leave at the borders when a label is being clamped
+	bool useMainCamera = true;	// Use the camera tagged MainCamera
+	Camera cameraToUse;	// Only use this if useMainCamera is false
+	private Camera cam;
+	private Transform thisTransform;
+	private Transform camTransform;
+
+	public void Start () {
+		thisTransform = transform;
+
+		if (useMainCamera) {
+			cam = Camera.main;
+		} else {
+			cam = cameraToUse;
+		}
+
+		camTransform = cam.transform;
+	}
+
+	public void Update () {
+		if (clampToScreen) {
+			var relativePosition = camTransform.InverseTransformPoint(target.position);
+			relativePosition.z = Mathf.Max(relativePosition.z, 1.0f);
+			thisTransform.position = cam.WorldToViewportPoint(camTransform.TransformPoint(relativePosition + offset));
+			thisTransform.position = new Vector3(Mathf.Clamp(thisTransform.position.x, clampBorderSize, 1.0f - clampBorderSize),
+				Mathf.Clamp(thisTransform.position.y, clampBorderSize, 1.0f - clampBorderSize),
+				thisTransform.position.z);
+		}
+		else {
+			thisTransform.position = cam.WorldToViewportPoint(target.position + offset);
+		}
+	}
+}
