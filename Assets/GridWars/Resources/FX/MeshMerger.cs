bc5b7fbebb78d6804314262aea24fded2db4df61
Steve Dekorte <steve@dekorte.com>
2016-11-03 17:49:22 -0700

    adding mesh merger code but haven't integrated it yet

---
+++ Assets/GridWars/Resources/FX/MeshMerger.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class MeshMerger : MonoBehaviour {
+
+	private Dictionary<Material, List<MeshFilter>> meshes = null;
+	private bool foundPrefab = false;
+
+	public void Run () {
+		string goName = gameObject.name;
+		Debug.Log(gameObject.name + " MeshMerger");
+
+		Transform originalTransform = transform;
+		transform.position = Vector3.zero;
+		transform.rotation = Quaternion.identity;
+
+		meshes = new Dictionary<Material, List<MeshFilter>>();
+
+		// walk transforms and find meshes
+		// sort into dict by material
+		// don't go into children with transforms labeled as rotation points
+		WalkTransform(transform);
+
+		// walk dict and merge meshes with similar material
+		if (!foundPrefab) {
+			MergeMeshesForMaterials();
+		} else {
+			Debug.Log(gameObject.name + " MeshMerger BAILED");
+		}
+
+		transform.position = originalTransform.position;
+		transform.rotation = originalTransform.rotation;
+		//Destroy(this);
+
+	}
+
+	void WalkTransform(Transform t) {
+		if (t.name == "pivot") {
+			return;
+		}
+			
+		AddMeshForTransform(t);
+
+		foreach (Transform child in t) {
+			WalkTransform(child);
+			if (foundPrefab) {
+				break;
+			}
+		}
+	}
+
+	void AddMeshForTransform(Transform t) {
+		MeshFilter meshFilter = t.GetComponent<MeshFilter>();
+		Renderer renderer = t.GetComponent<Renderer>();
+
+		if (meshFilter != null && renderer != null) {
+			Material mat = null;
+
+			if (renderer.sharedMaterials != null) {
+				foundPrefab = true;
+				return;
+			}
+
+			try {
+				mat = renderer.material;
+				//mat = renderer.sharedMaterial;
+			} catch {
+				Debug.Log(gameObject.name + " MeshMerger: found a prefab");
+				foundPrefab = true;
+				return;
+			}
+
+			if (mat != null) {
+				if (!meshes.ContainsKey(mat)) {
+					meshes[mat] = new List<MeshFilter>();
+				}
+				meshes[mat].Add(meshFilter);
+			} else {
+				Debug.Log(gameObject.name + " MeshMerger: missing material on renderer");
+				foundPrefab = true;
+				return;
+			}
+		}
+	}
+
+	void MergeMeshesForMaterials() {
+		//Debug.Log(gameObject.name + " merging " + meshes.Count + " material meshes");
+
+		foreach(KeyValuePair<Material, List<MeshFilter>> entry in meshes) {
+			//List<MeshFilter> mfs = entry.Value;
+			MergeMaterialMeshes(entry.Key, entry.Value);
+		}
+	}
+
+	void MergeMaterialMeshes(Material material, List<MeshFilter> mfs) {
+
+		CombineInstance[] combine = new CombineInstance[mfs.Count];
+
+		// add old meshes to combine
+		for (int i = 0; i < mfs.Count; i++) {
+			MeshFilter meshFilter = mfs[i];
+			//combine[i].mesh = meshFilter.sharedMesh;
+			combine[i].mesh = meshFilter.mesh;
+			combine[i].transform = meshFilter.transform.localToWorldMatrix;
+		}
+
+		// add a child game object to put our new mesh in
+		GameObject newGo = new GameObject("merged mesh for material " + material.name);
+		newGo.transform.parent = transform;
+
+		MeshFilter newMeshFilter = newGo.AddComponent<MeshFilter>();
+		newMeshFilter.mesh = new Mesh();
+		newMeshFilter.mesh.CombineMeshes(combine); // combine meshes
+
+		MeshRenderer renderer = newGo.AddComponent<MeshRenderer>();
+		renderer.material = material; // set material
+			
+		// remove old mesh filters and renderers
+		for (int i = 0; i < mfs.Count; i++) {
+			MeshFilter mf = mfs[i];
+			Renderer r = mf.gameObject.GetComponent<Renderer>();
+			//Destroy(r);
+			//Destroy(mf);
+			r.enabled = false;
+			//mf.enabled = false;
+		}
+
+		Debug.Log(gameObject.name + " " + material.name + " merged " + mfs.Count + " meshes");
+	}
+}
