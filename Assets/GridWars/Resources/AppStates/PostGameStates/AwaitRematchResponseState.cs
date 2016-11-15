8d7abba096f0f59b22709cbc1fde6be23ab0b640
Rich Collins <richcollins@gmail.com>
2016-11-14 18:23:28 -0800

    wait a few seconds before showing Leave while waiting for rematch

--- Assets/GridWars/Resources/AppStates/PostGameStates/AwaitRematchResponseState.cs
+++ Assets/GridWars/Resources/AppStates/PostGameStates/AwaitRematchResponseState.cs
@@ -3,9 +3,28 @@ using System.Collections;
 using AssemblyCSharp;
 
 public class AwaitRematchResponseState : PostGameSubState {
+	Timer timer;
+
 	public override void EnterFrom(AppState state) {
 		base.EnterFrom(state);
 
+		timer = app.timerCenter.NewTimer();
+		timer.action = ShowLeave;
+		timer.timeout = 5;
+		timer.Start();
+
+		app.ResetMenu();
+		menu.AddItem(UI.ActivityIndicator("WAITING FOR RESPONSE"));
+		menu.Show();
+	}
+
+	public override void WillExit() {
+		base.WillExit();
+
+		timer.Cancel();
+	}
+
+	void ShowLeave() {
 		app.ResetMenu();
 		menu.AddItem(UI.ActivityIndicator("WAITING FOR RESPONSE"));
 		menu.AddItem(UI.MenuItem("Leave", postGameState.Leave), true);
