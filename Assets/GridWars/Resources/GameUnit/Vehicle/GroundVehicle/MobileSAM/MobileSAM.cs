223036ccaa4e6d640424e81caef9569adf8f32a4
Steve Dekorte <steve@dekorte.com>
2016-12-13 17:21:03 -0800

    SAM vet cost adjust

--- Assets/GridWars/Resources/GameUnit/Vehicle/GroundVehicle/MobileSAM/MobileSAM.cs
+++ Assets/GridWars/Resources/GameUnit/Vehicle/GroundVehicle/MobileSAM/MobileSAM.cs
@@ -6,7 +6,7 @@ public class MobileSAM : GroundVehicle {
 
 	public override void Awake() {
 		base.Awake();
-		powerCostPerLevel = new float[] { 8f, 7f*2f, float.MaxValue };
+		powerCostPerLevel = new float[] { 8f, 7f*1.5f, float.MaxValue };
 	}
 
 	//NetworkObject
