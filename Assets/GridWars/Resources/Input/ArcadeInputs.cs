81cbf38fd69e108118cc9d539857df8c4152214d
Rich Collins <richcollins@gmail.com>
2019-03-19 11:31:22 -0700

    Fix arcade button bindings

--- Assets/GridWars/Resources/Input/ArcadeInputs.cs
+++ Assets/GridWars/Resources/Input/ArcadeInputs.cs
@@ -20,38 +20,38 @@ public class ArcadeInputs : PlayerActionSet {
 
     public void AddBindings() {
         if (isLeftPlayer) {
-            releaseMobileSam.AddDefaultBinding(InputControlType.Action1);
-            releaseTanker.AddDefaultBinding(InputControlType.Action2);
-            releaseTank.AddDefaultBinding(InputControlType.Action3);
-            releaseChopper.AddDefaultBinding(InputControlType.Action4);
+            releaseMobileSam.AddDefaultBinding(InputControlType.Button4);
+            releaseTanker.AddDefaultBinding(InputControlType.Button5);
+            releaseTank.AddDefaultBinding(InputControlType.Button6);
+            releaseChopper.AddDefaultBinding(InputControlType.Button7);
             releaseMobileSam.AddDefaultBinding(Key.Q);
             releaseTanker.AddDefaultBinding(Key.W);
             releaseTank.AddDefaultBinding(Key.E);
             releaseChopper.AddDefaultBinding(Key.R);
 
-            anyButton.AddDefaultBinding(InputControlType.Action1);
-            anyButton.AddDefaultBinding(InputControlType.Action2);
-            anyButton.AddDefaultBinding(InputControlType.Action3);
-            anyButton.AddDefaultBinding(InputControlType.Action4);
+            anyButton.AddDefaultBinding(InputControlType.Button4);
+            anyButton.AddDefaultBinding(InputControlType.Button5);
+            anyButton.AddDefaultBinding(InputControlType.Button6);
+            anyButton.AddDefaultBinding(InputControlType.Button7);
             anyButton.AddDefaultBinding(Key.Q);
             anyButton.AddDefaultBinding(Key.W);
             anyButton.AddDefaultBinding(Key.E);
             anyButton.AddDefaultBinding(Key.R);
         }
         else {
-            releaseMobileSam.AddDefaultBinding(InputControlType.Action5);
-            releaseTanker.AddDefaultBinding(InputControlType.Action6);
-            releaseTank.AddDefaultBinding(InputControlType.Action7);
-            releaseChopper.AddDefaultBinding(InputControlType.Action8);
+            releaseMobileSam.AddDefaultBinding(InputControlType.Button0);
+            releaseTanker.AddDefaultBinding(InputControlType.Button1);
+            releaseTank.AddDefaultBinding(InputControlType.Button2);
+            releaseChopper.AddDefaultBinding(InputControlType.Button3);
             releaseMobileSam.AddDefaultBinding(Key.U);
             releaseTanker.AddDefaultBinding(Key.I);
             releaseTank.AddDefaultBinding(Key.O);
             releaseChopper.AddDefaultBinding(Key.P);
 
-            anyButton.AddDefaultBinding(InputControlType.Action5);
-            anyButton.AddDefaultBinding(InputControlType.Action6);
-            anyButton.AddDefaultBinding(InputControlType.Action7);
-            anyButton.AddDefaultBinding(InputControlType.Action8);
+            anyButton.AddDefaultBinding(InputControlType.Button0);
+            anyButton.AddDefaultBinding(InputControlType.Button1);
+            anyButton.AddDefaultBinding(InputControlType.Button2);
+            anyButton.AddDefaultBinding(InputControlType.Button3);
             anyButton.AddDefaultBinding(Key.U);
             anyButton.AddDefaultBinding(Key.I);
             anyButton.AddDefaultBinding(Key.O);
