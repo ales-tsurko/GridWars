ca8734d121f83dc484d5d14423a5f2c023d680a8
Ales Tsurko <ales.tsurko@gmail.com>
2021-09-16 12:53:29 +0300

    Update

--- Assets/Arcade/Arcade.cs
+++ Assets/Arcade/Arcade.cs
@@ -7,7 +7,7 @@ public class Arcade : MonoBehaviour {
 
 	private float speed = 0.03F;
 	public GameObject screen;
-	public MovieTexture movieTexture;
+	// public MovieTexture movieTexture;
 	private MeshRenderer meshRenderer;
 
 	void Start() {
@@ -15,15 +15,21 @@ public class Arcade : MonoBehaviour {
 		Material material = meshRenderer.material;
 		//SetTexture("_EMISSION", movieTexture);
 		//MovieTexture movie = (MovieTexture)m.mainTexture;
+        var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.playOnAwake = true;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = meshRenderer;
+        videoPlayer.targetMaterialProperty = "_MainTex";
+        videoPlayer.Play();
 
 
-		MovieTexture newMovie = (MovieTexture)material.GetTexture("_EmissionMap");
-		material.SetTexture("_MainTex", newMovie);
-		material.SetTexture("_EmissionMap", newMovie);
-
-		((MovieTexture)meshRenderer.material.mainTexture).Play();
-		((MovieTexture)meshRenderer.material.mainTexture).loop = true;
-		newMovie.Play();
+		// MovieTexture newMovie = (MovieTexture)material.GetTexture("_EmissionMap");
+		// material.SetTexture("_MainTex", newMovie);
+		// material.SetTexture("_EmissionMap", newMovie);
+// 
+		// ((MovieTexture)meshRenderer.material.mainTexture).Play();
+		// ((MovieTexture)meshRenderer.material.mainTexture).loop = true;
+		// newMovie.Play();
 	}
 
 	void Update() {
